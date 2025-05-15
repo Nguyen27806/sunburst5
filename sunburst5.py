45edc80e75c69282c368a831166f3b189c6b6b09
Nguyen27806 <106240064@student.vgu.edu.vn>
2025-05-15 09:32:01 +0700

    Update sunburst5.py

--- sunburst5.py
+++ sunburst5.py
@@ -38,7 +38,7 @@ if uploaded_file is not None:
     )
 
     # Show only first level initially
-    fig.update_traces(maxdepth=1)
+    fig.update_traces(maxdepth=2)
 
     # Display the chart
     st.plotly_chart(fig)
