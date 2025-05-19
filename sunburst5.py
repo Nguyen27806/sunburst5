739e6e8cb387d23e7916a520d78d18a71e27b593
Nguyen27806 <106240064@student.vgu.edu.vn>
2025-05-19 20:56:56 +0700

    Update sunburst5.py

--- sunburst5.py
+++ sunburst5.py
@@ -31,6 +31,7 @@ if uploaded_file is not None:
         total = sunburst_data['Count'].sum()
         sunburst_data['Percentage'] = (sunburst_data['Count'] / total * 100).round(2)
 
+        # Label cho các tầng
         sunburst_data['Entrepreneurship_Label'] = sunburst_data['Entrepreneurship'] + ' (' + (
             sunburst_data.groupby('Entrepreneurship')['Count'].transform(lambda x: round(x.sum() / total * 100, 1)).astype(str)
         ) + '%)'
@@ -42,12 +43,12 @@ if uploaded_file is not None:
         sunburst_data['Salary_Label'] = sunburst_data['Salary_Group'] + '\n' + sunburst_data['Percentage'].astype(str) + '%'
 
         if color_mode == "Color by Salary Group":
-            # YES = xanh lá cây
+            # YES = xanh lá
             yes_fields = sunburst_data[sunburst_data['Entrepreneurship'] == 'Yes']['Field_of_Study'].unique()
             yes_colors = px.colors.sample_colorscale("Greens", [i / max(1, len(yes_fields) - 1) for i in range(len(yes_fields))])
             field_color_map = {('Yes', field): yes_colors[i] for i, field in enumerate(yes_fields)}
 
-            # NO = bảng màu đỏ
+            # NO = đỏ
             red_palette = [
                 "#F8B5B5", "#F78C8C", "#F65C5C", "#F43131", "#F20000",
                 "#DB8A8A", "#DA6363", "#D63C3C", "#D11111", "#BD0000",
@@ -57,18 +58,20 @@ if uploaded_file is not None:
             for i, field in enumerate(no_fields):
                 field_color_map[('No', field)] = red_palette[i % len(red_palette)]
 
-            # Gán nhãn duy nhất để vẽ màu đúng
-            sunburst_data['Color_Key'] = sunburst_data['Entrepreneurship'] + " | " + sunburst_data['Field_of_Study']
-            sunburst_data['Color_Assign'] = sunburst_data.apply(
-                lambda row: field_color_map.get((row['Entrepreneurship'], row['Field_of_Study']), "#000000"),
-                axis=1
-            )
+            # 💡 Gán màu cho nhánh chính Yes / No
+            label_yes = sunburst_data[sunburst_data['Entrepreneurship'] == 'Yes']['Entrepreneurship_Label'].iloc[0]
+            label_no = sunburst_data[sunburst_data['Entrepreneurship'] == 'No']['Entrepreneurship_Label'].iloc[0]
 
-            # Tạo map ánh xạ màu
             color_discrete_map = {
-                key: sunburst_data[sunburst_data['Color_Key'] == key]['Color_Assign'].values[0]
-                for key in sunburst_data['Color_Key'].unique()
+                label_yes: "#2ECC71",  # xanh lá cho Yes
+                label_no: "#E74C3C",   # đỏ tươi cho No
             }
+
+            # Gán màu cho ngành con (Field_of_Study)
+            sunburst_data['Color_Key'] = sunburst_data['Entrepreneurship_Label'] + " / " + sunburst_data['Field_of_Study']
+            for _, row in sunburst_data.iterrows():
+                color_discrete_map[row['Entrepreneurship_Label'] + " / " + row['Field_of_Study']] = \
+                    field_color_map.get((row['Entrepreneurship'], row['Field_of_Study']), "#000000")
 
             fig = px.sunburst(
                 sunburst_data,
